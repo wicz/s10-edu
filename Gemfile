c034b7a7a90915c3a52edf0153543e65475eeb18
Vinicius Horewicz <vinicius@horewi.cz>
2012-01-18 11:46:53 -0200

    Add project foundation

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source :rubygems
+
+group :test do
+  gem 'rspec', '~> 2.8'
+  gem 'guard-rspec', '~> 0.6'
+end
---
+++ spec/rps_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Rps do
+  it "is a module" do
+    Rps.should be_a(Module)
+  end
+end
