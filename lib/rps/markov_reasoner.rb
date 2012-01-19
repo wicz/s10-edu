f8308d137cdf27c40fbe4a62703834e107cb3255
Vinicius Horewicz <vinicius@horewi.cz>
2012-01-19 00:25:53 -0200

    The brains!

---
+++ lib/rps/markov_reasoner.rb
@@ -0,0 +1,45 @@
+module Rps
+  class MarkovReasoner
+    UnknownEvent = Class.new(StandardError)
+
+    attr_accessor :states, :events, :matrix
+
+    def initialize(states, initial_state = nil)
+      @states = states
+      @events = []
+      @events << initial_state if initial_state
+      setup_matrix
+    end
+
+    def setup_matrix
+      @matrix = {}
+      @states.each do |i|
+        @matrix[i] = {}
+        @states.each do |j|
+          @matrix[i][j] = 0
+        end
+      end
+      @matrix
+    end
+
+    def last_event
+      @events.last || @states.sample
+    end
+
+    def add_event event
+      check_event event
+      @matrix[last_event][event] += 1
+      @events << event
+    end
+
+    def estimate_next_events
+      max = @matrix[last_event].values.max
+      possibilities = @matrix[last_event].select { |k, v| v >= max }
+      possibilities.keys
+    end
+
+    def check_event event
+      raise UnknownEvent unless @states.include? event
+    end
+  end
+end
---
+++ spec/rps/markov_reasoner_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+describe Rps::MarkovReasoner do
+
+  let(:states) { %w{ r p s }.map(&:to_sym) }
+  let(:reasoner) { Rps::MarkovReasoner.new states }
+
+  describe "#new" do
+    it "needs states on initialization" do
+      reasoner.states.should be(states)
+    end
+
+    it "can have initial state" do
+      reasoner = Rps::MarkovReasoner.new([:r, :p, :s], :r)
+      reasoner.events.should eq([:r])
+    end
+
+    it "setups matrix" do
+      reasoner.matrix.keys.should eq(states)
+    end
+  end
+
+  describe "#setup_matrix" do
+    it "sets all elements to zero" do
+      reasoner.setup_matrix
+      states.each do |state|
+        reasoner.matrix[state].keys.should eq(states)
+        reasoner.matrix[state].values.should eq([0, 0, 0])
+      end
+    end
+  end
+
+  describe "#add_event" do
+    it "can't add unknown event" do
+      expect { reasoner.add_event('k') }.to raise_error Rps::MarkovReasoner::UnknownEvent
+    end
+
+    it "append event" do
+      reasoner.add_event :r
+      reasoner.events.should eq([:r])
+    end
+
+    describe "given the last and current plays are paper and rock respectively" do
+      it "increment the transition from rock to paper by 1" do
+        reasoner.events = [:p]
+        reasoner.add_event(:r)
+        reasoner.matrix[:p][:r].should eq(1)
+      end
+    end
+  end
+
+  describe "#estimate_next_events" do
+    before(:all) do
+      reasoner.matrix = {
+        r: { r: 3, p: 3, s: 2 },
+        p: { r: 2, p: 2, s: 1 },
+        s: { r: 1, p: 0, s: 0 }
+      }
+    end
+    it "estimates rock if current is scissors" do
+      reasoner.events = [:s]
+      reasoner.estimate_next_events.should eq([:r])
+    end
+
+    it "estimates rock or paper if current is rock" do
+      reasoner.events = [:r]
+      reasoner.estimate_next_events.should eq([:r, :p])
+    end
+  end
+
+  describe "#last_event" do
+    it "gets random sample if empty events" do
+      reasoner.states.should_receive :sample
+      reasoner.last_event
+    end
+  end
+end
